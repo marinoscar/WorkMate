a4ad7df910faa61ae4d144cf447b92a7f126df54
Oscar Marin <oscar@marin.cr>
2025-01-03 09:11:41 -0600

    Update Program.cs
    updated the settings

--- src/Luval.WorkMate.UI/Program.cs
+++ src/Luval.WorkMate.UI/Program.cs
@@ -1,3 +1,8 @@
+using Luval.AuthMate.Core;
+using Luval.AuthMate.Infrastructure.Configuration;
+using Luval.AuthMate.Infrastructure.Data;
+using Luval.AuthMate.Infrastructure.Logging;
+using Luval.AuthMate.Sqlite;
 using Luval.WorkMate.UI.Components;
 using Microsoft.FluentUI.AspNetCore.Components;
 
@@ -9,10 +14,41 @@ namespace Luval.WorkMate.UI
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var config = builder.Configuration;
+
             // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
             builder.Services.AddFluentUIComponents();
+
+            //Add logging services is a required dependency for AuthMate
+            builder.Services.AddLogging();
+
+            //Add the controllers and the http client and context accessor
+            builder.Services.AddControllers();
+            builder.Services.AddHttpClient();
+            builder.Services.AddHttpContextAccessor();
+
+            //Add the AuthMate services
+            builder.Services.AddAuthMateServices(
+                //The key to use for the bearing token implementation
+                config["AuthMate:BearingTokenKey"],
+                (s) => {
+                    //returns a local instance of Sqlite
+                    //replace this with your own implementation of Postgres, MySql, SqlServer, etc
+                    return new SqliteAuthMateContext();
+                });
+
+            //Add the AuthMate Google OAuth provider
+            builder.Services.AddAuthMateGoogleAuth(new GoogleOAuthConfiguration()
+            {
+                // client id from your config file
+                ClientId = config["OAuthProviders:Google:ClientId"] ?? throw new ArgumentNullException("The Google client id is required"),
+                // the client secret from your config file
+                ClientSecret = config["OAuthProviders:Google:ClientSecret"] ?? throw new ArgumentNullException("The Google client secret is required"),
+                // set the login path in the controller and pass the provider name
+                LoginPath = "/api/auth",
+            });
 
             var app = builder.Build();
 
@@ -26,11 +62,28 @@ namespace Luval.WorkMate.UI
 
             app.UseHttpsRedirection();
 
+            //Add the authentication and authorization
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            //Map the controllers and the razor components
+            app.MapControllers();
+
             app.UseStaticFiles();
             app.UseAntiforgery();
 
             app.MapRazorComponents<App>()
                 .AddInteractiveServerRenderMode();
+
+            //Inialize the app database using Sqlite
+            var contextHelper = new AuthMateContextHelper(
+                new SqliteAuthMateContext(),
+                new ColorConsoleLogger<AuthMateContextHelper>());
+            //Makes sure the db is created, then initializes the db with the owner email
+            //and required initial records
+            contextHelper.InitializeDbAsync(config["OAuthProviders:Google:OwnerEmail"] ?? "someone@gmail.com")
+                .GetAwaiter()
+                .GetResult();
 
             app.Run();
         }
