191eeb794bf46063befb0b8be675e4c89784375a
Oscar Marin <oscar@marin.cr>
2025-01-04 20:58:48 -0600

    Create TodoSetDto.cs
    class to handle a data transfer object

---
+++ src/Luval.WorkMate/Infrastructure/Data/TodoSetDto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Graph.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luval.WorkMate.Infrastructure.Data
+{
+    public class TodoSetDto
+    {
+        public string ListId { get; set; } = default!;
+        public string DisplanyName { get; set; } = default!;
+        public List<TodoTask> Tasks { get; set; } = new List<TodoTask>();
+    }
+}
