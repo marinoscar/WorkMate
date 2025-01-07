a75f08073a7584f33458233e283f08fe4b040933
Oscar Marin <oscar@marin.cr>
2025-01-06 18:30:02 -0600

    Create SubscriptionTimedService.cs

---
+++ src/Luval.WorkMate/Core/HostedService/SubscriptionTimedService.cs
@@ -0,0 +1,23 @@
+﻿using Luval.WorkMate.Core.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luval.WorkMate.Core.HostedService
+{
+    public class SubscriptionTimedService : TimedHostedService
+    {
+        public SubscriptionTimedService(SubscriptionService service, IConfiguration configuration, ILoggerFactory loggerFactory) : base(configuration, loggerFactory)
+        {
+        }
+
+        public override Task DoWorkAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
