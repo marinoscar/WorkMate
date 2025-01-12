bc146f8c54e3e4cd0f8cf5eb66b576066ebf5f25
Oscar Marin <oscar@marin.cr>
2025-01-11 21:40:08 -0600

    Update EmailAttachmentTimeService.cs

--- src/Luval.WorkMate/Core/HostedService/EmailAttachmentTimeService.cs
+++ src/Luval.WorkMate/Core/HostedService/EmailAttachmentTimeService.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,7 +18,6 @@ namespace Luval.WorkMate.Core.HostedService
     public class EmailAttachmentTimeService : TimedHostedService
     {
         private readonly UniqueConcurrentQueue<string, ChangeNotification> _queue;
-        private EmailAttachmentService _service;
 
         public EmailAttachmentTimeService(IServiceProvider serviceProvider) : base(serviceProvider)
         {
@@ -25,22 +25,39 @@ namespace Luval.WorkMate.Core.HostedService
         }
         public override async Task DoWorkAsync(CancellationToken cancellationToken)
         {
-            if (_service == null)
-                _service = new EmailAttachmentService(ServiceScope.ServiceProvider.GetRequiredService<ILogger<EmailAttachmentService>>(),
-                        ServiceScope.ServiceProvider.GetRequiredService<GenAIBotService>(),
-                        ServiceScope.ServiceProvider.GetRequiredService<EmailService>(),
-                        ServiceScope.ServiceProvider.GetRequiredService<TodoService>(),
-                        ServiceScope.ServiceProvider.GetRequiredService<OneNoteService>());
+            Logger.LogDebug($"Total items in queue {_queue.Count}");
+            var taskList = new List<Task>()
+            {
+                RunProcessAsync(ServiceProvider, 1, cancellationToken),
+                RunProcessAsync(ServiceProvider, 2, cancellationToken),
+                RunProcessAsync(ServiceProvider, 3, cancellationToken),
+                RunProcessAsync(ServiceProvider, 4, cancellationToken),
+                RunProcessAsync(ServiceProvider, 5, cancellationToken),
+            };
 
-            Logger.LogDebug($"Total items in queue { _queue.Count }");
-
-            if (!_queue.TryDequeue(out var changeNotification)) return;
-
-            var id = changeNotification.Resource.GetResourceId();
-
-            Logger.LogInformation($"Processing email attachment with Id: {id}");
-
-            await _service.ProcessEmailAttachmentAsync(id, cancellationToken);
+            await Task.WhenAll(taskList);
         }
+
+        public async Task RunProcessAsync(IServiceProvider serviceProvider, int threadNo, CancellationToken cancellationToken)
+        {
+            if (!_queue.TryDequeue(out var changeNotification)) return;
+            if(changeNotification == null || string.IsNullOrEmpty(changeNotification.Resource)) return;
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var service = new EmailAttachmentService(scope.ServiceProvider.GetRequiredService<ILogger<EmailAttachmentService>>(),
+                            scope.ServiceProvider.GetRequiredService<GenAIBotService>(),
+                            scope.ServiceProvider.GetRequiredService<EmailService>(),
+                            scope.ServiceProvider.GetRequiredService<TodoService>(),
+                            scope.ServiceProvider.GetRequiredService<OneNoteService>());
+
+                var id = changeNotification.Resource.GetResourceId();
+
+                Logger.LogInformation($"Thread: {threadNo} Processing email attachment with Id: {id}");
+
+                await service.ProcessEmailAttachmentAsync(id, cancellationToken);
+            }
+        }
+
     }
 }
